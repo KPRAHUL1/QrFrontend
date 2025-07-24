68609a062db8ef6516be792e2c6d5b7567a1c883
Rahul K <rahulsachin07314@gmail.com>
2025-07-24 14:50:20 +0530

    edit

--- src/page/QrPage.jsx
+++ src/page/QrPage.jsx
@@ -5,7 +5,7 @@ const QRPage = () => {
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <div className="p-6 bg-white rounded-xl shadow-lg">
         <QRCodeCanvas
-          value="https://kgdkbzz7-5173.inc1.devtunnels.ms/register" // This should point to your registration form URL
+          value="https://genzteam.netlify.app/register" // This should point to your registration form URL
           size={200}
           level="H"
           fgColor="#000000"
